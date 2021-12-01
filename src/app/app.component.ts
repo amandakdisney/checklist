3d2fee43cec501852539791e7e4c2c0b7adc6c8f
amandakdisney <amandadisney93@gmail.com>
2021-11-30 19:55:05 -0500

    added saving and loading data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { ChecklistService } from './services/checklist.service';
 import { SplashScreen } from '@capacitor/splash-screen';
 import { StatusBar } from '@capacitor/status-bar';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,16 @@ import { StatusBar } from '@capacitor/status-bar';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {
+  constructor(
+    private checklistService: ChecklistService,
+    private storage: Storage
+  ) {
     this.initializeApp();
   }
 
   async initializeApp() {
+    await this.storage.create();
+    await this.checklistService.load();
     SplashScreen.hide().catch((err) => {
       console.warn(err);
     });
